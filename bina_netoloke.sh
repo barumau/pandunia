c36b4deae4d1e23c03fded05c73e4f2616e8acde
Risto Kupsala <risto@pandunia.info>
2020-08-19 14:50:52 +0300

    kitaba linke pa 'edita sa GitHub'

--- bina_netoloke.sh
+++ bina_netoloke.sh
@@ -1,22 +1,31 @@
-cd mkdocs
-cd cini
-mkdocs build
-cd ../engli
-mkdocs build
-cd ../espani
-mkdocs build
-cd ../esperanti
-mkdocs build
-cd ../fransi
-mkdocs build
-cd ../niponi
-mkdocs build
-cd ../pandunia
-mkdocs build
-cd ../polski
-mkdocs build
-cd ../rusi
-mkdocs build
-cd ../suomi
-mkdocs build
+bina_MkDocs() {
+  cd mkdocs/$1
+  mkdocs build
+  cd ../..
+}
+
+muta_GitHub_dokumsanduke() {
+    grep -rl "edit\/master\/docs" html/$1 | xargs sed -i "s/edit\/master\/docs/edit\/master\/$1/g"
+}
+
+bina_netoloke() {
+    echo $1
+    bina_MkDocs $1
+    muta_GitHub_dokumsanduke $1
+}
+
+bina_pane() {
+    bina_netoloke cini
+    bina_netoloke engli
+    bina_netoloke espani
+    bina_netoloke esperanti
+    bina_netoloke fransi
+    bina_netoloke niponi
+    bina_netoloke pandunia
+    bina_netoloke polski
+    bina_netoloke rusi
+    bina_netoloke suomi
+}
+
+bina_pane
 
